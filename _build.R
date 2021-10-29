4f27b98f8a8357cb17769cbd4758f5ba7564ac5a
Lars Vilhuber <lars@vilhuber.com>
2021-10-28 23:05:31 -0400

    Lets see if this compiles in the cloud

---
+++ _build.R
@@ -0,0 +1,17 @@
+
+# for testing on Mac
+sysinf <- Sys.info()
+
+if ( sysinf['sysname'] == 'Darwin' ) {
+   Sys.setenv(RSTUDIO_PANDOC="/Applications/RStudio.app/Contents/MacOS/pandoc")
+}
+
+# setup a few things, if needed
+
+source("libraries.R")
+
+#run render
+
+rmarkdown::render("index.Rmd")
+rmarkdown::render("part2.Rmd")
+rmarkdown::render("part3.Rmd")
